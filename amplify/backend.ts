8ab2ac04ad6f93b6b4914ca0914003d04a1fb44d
huimin chen <huimin.chen.encore@gmail.com>
2025-01-08 13:28:37 +0100

    update

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
-import { deviceControl } from './function/deviceControl/resource';
+import { auth } from './auth/resource';
+import { data } from './data/resource';
 
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
 defineBackend({
   auth,
   data,
-  deviceControl,
 });
