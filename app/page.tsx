bcacda4a0241d16f31d8c23d09b76a2ac5ed0de6
huimin chen <huimin.chen.encore@gmail.com>
2025-01-09 11:54:44 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,6 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const [deviceData, setDeviceData] = useState<Array<Schema["Data"]["type"]>>(
-    []
-  );
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -24,25 +21,14 @@ export default function App() {
     });
   }
 
-  function getDeviceInfo() {
-    client.models.Data.observeQuery().subscribe({
-      next: (data) => setDeviceData([...data.items]),
-    });
-  }
-
   useEffect(() => {
     listTodos();
-    getDeviceInfo();
   }, []);
 
   function createTodo() {
     client.models.Todo.create({
       content: window.prompt("Todo content"),
     });
-  }
-
-  function addDevice() {
-    console.log("addDevice");
   }
 
   return (
@@ -61,22 +47,6 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
-      <h1>Device Information</h1>
-      <div className="device-info">
-        {deviceData.map((device) => (
-          <div key={device.id}>
-            <p>Battery: {device.battery}%</p>
-            <p>CO2: {device.co2} ppm</p>
-            <p>Humidity: {device.humidity}%</p>
-            <p>Light Level: {device.light_level}</p>
-            <p>PIR Status: {device.pir}</p>
-            <p>Pressure: {device.pressure} hPa</p>
-            <p>Temperature: {device.temperature}°C</p>
-            <p>TVOC: {device.tvoc} ppb</p>
-          </div>
-        ))}
-      </div>
-      <button onClick={addDevice}>Add device</button>
     </main>
   );
 }
